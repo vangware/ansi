685f9e1bf969c32f519c0cc96387d641224f6129
Lucas Ciruzzi <hi@lshi.ru>
2020-09-05 02:23:29 -0700

    :building_construction: change formats to be ignored when env ask for no colors.

---
+++ src/optionalFormat.ts
@@ -0,0 +1,11 @@
+import { format } from "./format";
+import { formattingEnabled } from "./formattingEnabled";
+
+/**
+ * Impure version of `format` which changes depending on the current NodeJS
+ * `process` environment.
+ * @param process NodeJS `process`.
+ */
+export const optionalFormat = formattingEnabled(process)
+	? format
+	: (_close: number) => (_open: number) => (source: string) => source;
