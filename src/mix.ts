4db4d7166c82ae76bfbe2d38bc25226584bec60d
Lucas Ciruzzi <hi@lshi.ru>
2021-04-11 19:20:15 -0700

    :arrow_up: dependency upgrade. :wrench: update `package.json` adding `lint:fix` script. :wrench: update `package.json` `docs` script. :building_construction: update code with new linting rules. :memo: update docs.

--- src/mix.ts
+++ src/mix.ts
@@ -1,10 +1,11 @@
-import { format } from "./format";
+import type { format } from "./format";
 
 /**
  * Composes formatters.
  * @param formatters Array of formatters to be composed.
  */
 export const mix = (
-	formatters: readonly ReturnType<ReturnType<typeof format>>[]
+	formatters: ReadonlyArray<ReturnType<ReturnType<typeof format>>>
 ) => (source: string) =>
+	// eslint-disable-next-line max-params
 	formatters.reduceRight((output, formatter) => formatter(output), source);
