96b5a9d021170a9d8567c1eaea56a81e5908e0a7
Luke Shiru <hi@lshi.ru>
2021-04-11 19:20:15 -0700

    :arrow_up: dependency upgrade. :wrench: update `package.json` adding `lint:fix` script. :wrench: update `package.json` `docs` script. :building_construction: update code with new linting rules. :memo: update docs.

--- src/formattingEnabled.ts
+++ src/formattingEnabled.ts
@@ -3,13 +3,18 @@
  * @param process NodeJS `process`.
  */
 export const formattingEnabled = ({
-	env: { FORCE_COLOR, NODE_DISABLE_COLORS, NO_COLOR, TERM } = {},
+	env: {
+		FORCE_COLOR: forceColor = "",
+		NODE_DISABLE_COLORS: nodeDisableColors = "",
+		NO_COLOR: noColor = "",
+		TERM: term
+	} = {},
 	stdout: { isTTY } = { isTTY: false }
 }: {
 	readonly env?: typeof process.env;
 	readonly stdout?: Pick<typeof process.stdout, "isTTY">;
 } = {}) =>
-	!NODE_DISABLE_COLORS &&
-	!NO_COLOR &&
-	TERM !== "dumb" &&
-	(!FORCE_COLOR || isTTY);
+	nodeDisableColors === "" &&
+	noColor === "" &&
+	term !== "dumb" &&
+	(forceColor === "" || isTTY);
