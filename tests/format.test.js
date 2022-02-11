3ef8817b13098898403c06bb809f5764d5cef25e
Luke Shiru <hi@luke.sh>
2022-02-11 12:05:06 -0800

    :arrow_up: dependency upgrade. :white_check_mark: update test to use a cheap `@vangware/test` approach. :white_check_mark: add tests for `normalizeString`.

--- tests/format.test.js
+++ tests/format.test.js
@@ -1,17 +1,17 @@
-import { deepEqual } from "node:assert/strict";
 import { format } from "../dist/format.js";
 
+/** @type {import(".").Tests<string>} */
 export default [
-	deepEqual(
-		format(2)(1)("foobar"),
-		"\x1b[1mfoobar\x1b[2m",
-		`Given a number 1, a number 2 and a string,
-	must return that string wrapped with ansi for 1 and for 2`,
-	),
-	deepEqual(
-		format(2)(1)`foobar ${10}`,
-		"\x1b[1mfoobar 10\x1b[2m",
-		`Given a number 1, a number 2 and a template literal,
-	must return that template literal as a string wrapped with ansi for 1 and for 2`,
-	),
+	{
+		given: "a number 1, a number 2 and a string",
+		must: "return that string wrapped with ansi for 1 and for 2",
+		received: format(2)(1)("foobar"),
+		wanted: "\x1b[1mfoobar\x1b[2m",
+	},
+	{
+		given: "a number 1, a number 2 and a template literal",
+		must: "return that template literal as a string wrapped with ansi for 1 and for 2",
+		received: format(2)(1)`foobar ${10}`,
+		wanted: "\x1b[1mfoobar 10\x1b[2m",
+	},
 ];
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,87 @@
-import "./ansi.test.js";
-import "./format.test.js";
-import "./formattingEnabled.test.js";
-import "./mix.test.js";
+import { deepEqual } from "node:assert/strict";
+import { readdir } from "node:fs/promises";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const testDirectory = dirname(fileURLToPath(import.meta.url));
+const testDirectoryRelative = testDirectory.replace(process.cwd(), ".");
+
+/**
+ * @template Value
+ * @typedef Test
+ * @property {string} given Given description.
+ * @property {string} must Must description.
+ * @property {Value} received Received value.
+ * @property {Value} wanted Wanted value.
+ */
+
+/**
+ * @template Value
+ * @typedef {import("@vangware/types").ReadOnlyArray<Test<Value>>} Tests
+ */
+
+void readdir(testDirectory)
+	.then(files =>
+		Promise.all(
+			files
+				.filter(file => file.endsWith(".test.js"))
+				.map(file =>
+					import(`${testDirectory}/${file}`).then(
+						({ default: tests = [] }) =>
+							Promise.allSettled([
+								`[TEST] ${testDirectoryRelative}/${file}`,
+								...(Array.isArray(tests)
+									? /** @type {Tests<unknown>} */ (tests).map(
+											({
+												given,
+												must,
+												received,
+												wanted,
+											}) => {
+												const description = `Given ${given}, must ${must}.`;
+
+												return new Promise(resolve =>
+													resolve(
+														deepEqual(
+															received,
+															wanted,
+														),
+													),
+												)
+													.then(
+														() =>
+															`[PASS] ${description}`,
+													)
+													.catch(() =>
+														Promise.reject(
+															`[FAIL] ${description}`,
+														),
+													);
+											},
+									  )
+									: []),
+							]),
+					),
+				),
+		),
+	)
+	.then(results => {
+		const flattenResults = results.flat();
+
+		// eslint-disable-next-line no-console
+		console.log(
+			flattenResults
+				.map(result =>
+					result.status === "fulfilled"
+						? result.value
+						: // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+						  `${result.reason}`,
+				)
+				.join("\n"),
+		);
+
+		return flattenResults.some(({ status }) => status === "rejected")
+			? 1
+			: 0;
+	})
+	.then(code => process.exit(code));
