0ddfef42196197c616a1487402c7995af81997cb
Lucas Ciruzzi <hi@lshi.ru>
2022-02-11 12:05:06 -0800

    :arrow_up: dependency upgrade. :white_check_mark: update test to use a cheap `@vangware/test` approach. :white_check_mark: add tests for `normalizeString`.

---
+++ tests/normalizeString.test.js
@@ -0,0 +1,17 @@
+import { normalizeString } from "../dist/normalizeString.js";
+
+/** @type {import(".").Tests<string>} */
+export default [
+	{
+		given: "a plain string",
+		must: "return that same string",
+		received: normalizeString("🟢"),
+		wanted: "🟢",
+	},
+	{
+		given: "a template string",
+		must: "return it as a string",
+		received: normalizeString`${13}`,
+		wanted: "13",
+	},
+];
