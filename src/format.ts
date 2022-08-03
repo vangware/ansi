725760bfb1ce001a051c5b1345a72fc5afd30ec4
Lou Cyx <git@lou.cx>
2022-08-03 14:01:37 -0700

    :arrow_up: dependency upgrade. :art: logo update. :bug: use `Formatter` less internally to get better docs.

--- src/format.ts
+++ src/format.ts
@@ -1,5 +1,5 @@
+import type { ReadOnlyArray } from "@vangware/types";
 import { ansi } from "./ansi.js";
-import type { Formatter } from "./Formatter.js";
 import { normalizeString } from "./normalizeString.js";
 
 /**
@@ -18,36 +18,9 @@ import { normalizeString } from "./normalizeString.js";
  */
 export const format =
 	<Close extends number>(close: Close) =>
-	/**
-	 * Function {@link format} with `close` string set.
-	 *
-	 * @example
-	 * ```typescript
-	 * const close13 = format(13);
-	 *
-	 * close13(42)("Vangware"); // "\x1b[42mVangware\x1b[13m"
-	 * // It can also be used as a tag function for tagged templates:
-	 * close13(42)`Vangware`; // "\x1b[42mVangware\x1b[13m"
-	 * ```
-	 * @param open Open value.
-	 * @returns Curried function with `open` and `close` in context.
-	 */
-	<Open extends number>(open: Open): Formatter =>
-	/**
-	 * Function {@link format} with `open` and `close` strings set.
-	 *
-	 * @example
-	 * ```typescript
-	 * const close13 = format(13);
-	 * const close13Open42 = format13(42);
-	 *
-	 * close13Open42("Vangware"); // "\x1b[42mVangware\x1b[13m"
-	 * // It can also be used as a tag function for tagged templates:
-	 * close13Open42`Vangware`; // "\x1b[42mVangware\x1b[13m"
-	 * ```
-	 * @param input Input `string` or `TemplateStringsArray`.
-	 * @param expressions Possible values passed to the template string.
-	 * @returns The `input` wrapped with `open` and `close`.
-	 */
-	(input, ...expressions) =>
+	<Open extends number>(open: Open) =>
+	(
+		input: Readonly<TemplateStringsArray> | string,
+		...expressions: ReadOnlyArray
+	) =>
 		`${ansi(open)}${normalizeString(input, ...expressions)}${ansi(close)}`;
